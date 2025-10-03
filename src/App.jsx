e6e6cfe2f9ce4acc6e39f874787919086501d43b
NC <samarthdevlibtech@gmail.com>
2025-10-03 22:43:02 +0530

    updated 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './common/Navbar'
 import Hero from './Components/HeroSection/Hero'
 import ServiceAsSoft from './Components/ServiceAsSoftware/ServiceAsSoft'
 import AnimatedResults from './Components/ProvenResults/ProvenResults'
 import UltraProProducts from './Components/UltraProProducts/UltraProProducts'
-// import IndustriesTransformed from './Components/IndustriesTransformed/IndustriesTransformed'
-import InnovationPillarsOne from './Components/innovationPillars/innovationPillarsOne'
+
 import ClientTestimonialsOne from './Components/ClientTestimonialsOne/ClientTestimonialsOne'
 import PartnerWithInovative from './Components/PartnerWithInovative/PartnerWithInovative'
 import FrequentlyAsked from './Components/FrequentlyAsked/FrequentlyAsked'
 import FooterOne from './common/FooterOne'
+import IndustriesTransformedOne from './Components/IndustriesTransformed/IndustriesTransformedOne'
 
-// import ProvenResults from './Components/ProvenResults/ProvenResults'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +26,7 @@ function App() {
       <AnimatedResults />
 
       <UltraProProducts/>
-      <InnovationPillarsOne/>
+      <IndustriesTransformedOne/>
       <ClientTestimonialsOne/>
       <PartnerWithInovative/>
       <FrequentlyAsked/>
